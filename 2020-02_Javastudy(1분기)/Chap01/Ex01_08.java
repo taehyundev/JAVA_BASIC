6791506e88034c788a9d128b9ef0209a64c1d395
taehyundev <50985723+taehyundev@users.noreply.github.com>
2020-02-04 19:21:20 +0900

    Update Ex01_08.java

--- 2020-02_Javastudy(1분기)/Chap01/Ex01_08.java
+++ 2020-02_Javastudy(1분기)/Chap01/Ex01_08.java
@@ -4,7 +4,8 @@ public class Ex01_08 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int a = 3, b=5;
+		System.out.println("두 수의 차는 " + ((a>b)?(a-b):(b-a));
 	}
 
 }
