bdb7f10de4234f69cbe48a1c9f1eea325e8de33d
taehyundev <50985723+taehyundev@users.noreply.github.com>
2020-02-08 23:11:17 +0900

    Update Ex05_03.java

--- 2020-02_Javastudy(1분기)/Chap05/Ex05_03.java
+++ 2020-02_Javastudy(1분기)/Chap05/Ex05_03.java
@@ -8,7 +8,7 @@ public class Ex05_03 {
 		// TODO Auto-generated method stub
 		HashMap<String, String> h = new HashMap<String, String>();
 		//<key, values>
-		h.put("apple", "»ç°ú");
+		h.put("apple", "ì‚¬ê³¼");
 		String kor = h.get("apple");//get(key)->values
 		System.out.println(kor);
 		
